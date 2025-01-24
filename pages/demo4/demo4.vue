9e59b91a79a7b14c45ca2dceff64732765549608
long <longbao7552@gmail.com>
2025-01-23 23:56:33 -0800

    lianbiao4

--- pages/demo4/demo4.vue
+++ pages/demo4/demo4.vue
@@ -5,13 +5,23 @@
 </template>
 
 <script setup>
-	const demoObj = uniCloud.importObject("demoObj");
-	const getData = async()=>{
-		let res = await demoObj.getUser();
-		console.log(res)
-	}
-	getData()
-
+	
+	// const demoObj = uniCloud.importObject("demoObj");
+	// const getData = async()=>{
+	// 	let res = await demoObj.getUser();
+	// 	console.log(res)
+	// }
+	// getData()
+	import { computed, ref } from 'vue';
+	const Data = ref([]);
+	
+	uni.request({
+	    url: 'https://env-00jxt113ioao.dev-hz.cloudbasefunction.cn/demoObj/getUser', 
+		data:{size:"2"},
+	    success: (res) => {
+	        console.log(res.data);
+	    }
+	});
 </script>
 
 <style lang="scss" scoped>
--- uniCloud-alipay/cloudfunctions/demoObj/index.obj.js
+++ uniCloud-alipay/cloudfunctions/demoObj/index.obj.js
@@ -4,8 +4,13 @@ module.exports = {
 
 	},
 	async getUser({size=3}=param){
-		// return size //这里的return是把size给了前端函数调用者，以promise对象的形式，给了前端的res  会打印出3
+		if(!isNaN(size)){
+			size=Number(size)
+		}else{
+			return {errMsg:"需要number类型"}
+		}
+		// return size //这里的return是把size给了前端函数调用者，以promise对象的形式，给了前端的res，会打印出3；用url方式返回给apiPost工具则会打印出对象，需要解构测绘识别
 		let res = await db.collection("default-user").limit(size).get();
-		return res  //这里的return是把res给了前端函数调用者，以promise对象的形式，给了前端的res  会打印出default-user数据
+		return res  //这里的return是把res给了前端函数调用者，以promise对象的形式，给了前端的res或者apipost工具，会打印出default-user数据
 	}
 }
