1a3328199106e1dff89fad2b483fa2f64211d8e5
long <longbao7552@gmail.com>
2025-01-23 23:27:30 -0800

    lianbiao3

---
+++ pages/demo4/demo4.vue
@@ -0,0 +1,19 @@
+<template>
+	<view class="">
+		
+	</view>
+</template>
+
+<script setup>
+	const demoObj = uniCloud.importObject("demoObj");
+	const getData = async()=>{
+		let res = await demoObj.getUser();
+		console.log(res)
+	}
+	getData()
+
+</script>
+
+<style lang="scss" scoped>
+	
+</style>
---
+++ uniCloud-alipay/cloudfunctions/demoObj/index.obj.js
@@ -0,0 +1,11 @@
+const db = uniCloud.database()
+module.exports = {
+	_before: function () { // 通用预处理器
+
+	},
+	async getUser({size=3}=param){
+		// return size //这里的return是把size给了前端函数调用者，以promise对象的形式，给了前端的res  会打印出3
+		let res = await db.collection("default-user").limit(size).get();
+		return res  //这里的return是把res给了前端函数调用者，以promise对象的形式，给了前端的res  会打印出default-user数据
+	}
+}
